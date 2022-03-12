689441a3c4a525956ce96925adbc646d69ef7eac
Bartłomiej Ściseł <bartlomiej.scisel.stud@pw.edu.pl>
2022-03-12 18:35:06 +0100

    New tests added for enqueue to full queue and dequeue from empty queue. Tests rewritten for SetUp and TearDown functions.

--- tests/source/queue_tests.cpp
+++ tests/source/queue_tests.cpp
@@ -1,15 +1,36 @@
 #include "gtest/gtest.h"
 #include "Queue.h"
 
-TEST(queue, test_one) {
-    Queue<int, 10> queue;
-    queue.enqueue(20);
+class QueueTest : public ::testing::Test {
+protected:
+    Queue<int, 4> queue;
+    void SetUp() override {
+        queue.enqueue(10);
+        queue.enqueue(20);
+    }
+
+    void TearDown() override {
+
+    }
+};
+
+
+TEST_F(QueueTest, basic_functionality) {
+    int dequeued = queue.dequeue();
+    EXPECT_EQ(dequeued, 10);
+    dequeued = queue.dequeue();
+    EXPECT_EQ(dequeued, 20);
+}
+
+TEST_F(QueueTest, dequeue_empty) {
+    queue.dequeue();
+    queue.dequeue();
+    ASSERT_THROW(queue.dequeue(), QueueIsEmpty);
+}
+
+
+TEST_F(QueueTest, queue_full) {
     queue.enqueue(30);
     queue.enqueue(40);
-    int dequeued = queue.dequeue();
-    EXPECT_EQ(dequeued, 20);
-    dequeued = queue.dequeue();
-    EXPECT_EQ(dequeued, 30);
-    dequeued = queue.dequeue();
-    EXPECT_EQ(dequeued, 40);
+    ASSERT_THROW(queue.enqueue(50), QueueIsFull);
 }
