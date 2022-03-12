d74f766cb134436d23aa1642e76920882952b3e3
Bartłomiej Ściseł <bartlomiej.scisel.stud@pw.edu.pl>
2022-03-12 18:35:50 +0100

    Added new exceptions for queue empty/full cases.

---
+++ data_structures/include/Exceptions.h
@@ -0,0 +1,18 @@
+#ifndef DATA_STRUCTURES_EXCEPTIONS_H
+#define DATA_STRUCTURES_EXCEPTIONS_H
+
+#include "exception"
+
+struct QueueIsEmpty : public std::exception {
+    [[nodiscard]] const char *what() const noexcept override {
+        return "Attempting to remove an object from an empty queue.";
+    }
+};
+
+struct QueueIsFull : public std::exception {
+    [[nodiscard]] const char *what() const noexcept override {
+        return "Attempting to add an object to the full queue.";
+    }
+};
+
+#endif
--- data_structures/include/Queue.h
+++ data_structures/include/Queue.h
@@ -1,29 +1,41 @@
 #ifndef DATA_STRUCTURES_QUEUE_H
 #define DATA_STRUCTURES_QUEUE_H
 
-template <typename T, size_t max_elements>
+#include "Exceptions.h"
+
+template<typename T, size_t max_elements>
 class Queue {
     size_t head = 0;
     size_t tail = 0;
-    T* array;
+    size_t elementsCount = 0;
+    T *array;
 public:
     Queue() : array{new T[max_elements]} {}
+
     ~Queue() {
         delete[] array;
     }
+
     void enqueue(T object) {
+        if (elementsCount == max_elements)
+            throw QueueIsFull();
         array[tail] = object;
         if (tail == max_elements)
             tail = 1;
         else
             tail++;
+        elementsCount++;
     }
+
     T dequeue() {
+        if (elementsCount == 0)
+            throw QueueIsEmpty();
         T object = array[head];
         if (head == max_elements)
             head = 1;
         else
             head++;
+        elementsCount--;
         return object;
     }
 };
