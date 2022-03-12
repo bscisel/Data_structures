32d86f435ccd97f717a0d2e3cd11216efc1e43b7
Bartłomiej Ściseł <bartlomiej.scisel.stud@pw.edu.pl>
2022-03-12 20:30:05 +0100

    Added new test when indexes are wrapping.

--- tests/source/queue_tests.cpp
+++ tests/source/queue_tests.cpp
@@ -17,14 +17,17 @@ protected:
 
 TEST_F(QueueTest, basic_functionality) {
     EXPECT_EQ(queue.rear(), 10);
+    EXPECT_EQ(queue.front(), 20);
     int dequeued = queue.dequeue();
     EXPECT_EQ(dequeued, 10);
     EXPECT_EQ(queue.rear(), 20);
+    EXPECT_EQ(queue.front(), 20);
     EXPECT_FALSE(queue.is_empty());
     EXPECT_FALSE(queue.is_full());
     dequeued = queue.dequeue();
     EXPECT_EQ(dequeued, 20);
     EXPECT_TRUE(queue.is_empty());
+    EXPECT_FALSE(queue.is_full());
 }
 
 TEST_F(QueueTest, dequeue_empty) {
@@ -34,8 +37,18 @@ TEST_F(QueueTest, dequeue_empty) {
 }
 
 
-TEST_F(QueueTest, queue_full) {
+TEST_F(QueueTest, enqueue_full) {
     queue.enqueue(30);
     queue.enqueue(40);
     ASSERT_THROW(queue.enqueue(50), QueueIsFull);
+}
+
+TEST_F(QueueTest, get_full_then_dequeue_enqueue) {
+    queue.enqueue(30);
+    queue.enqueue(40);
+    ASSERT_EQ(queue.dequeue(), 10);
+    queue.enqueue(50);
+    ASSERT_EQ(queue.dequeue(), 20);
+    ASSERT_EQ(queue.front(), 50);
+    ASSERT_EQ(queue.rear(), 30);
 }
